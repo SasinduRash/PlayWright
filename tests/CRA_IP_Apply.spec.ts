f1d3034d89c83a02b35c6a60ed36c8aad0a6f879
Sasindu Rashmika <133813189+SasinduRash@users.noreply.github.com>
2026-02-18 11:53:06 +0530

    Credit Rating Agency license apply and approval flow

--- tests/CRA_IP_Apply.spec.ts
+++ tests/CRA_IP_Apply.spec.ts
@@ -45,15 +45,27 @@ test('Apply Credit Rating Agency license with valid data', async ({ page }) => {
   await page.locator('section-card').filter({ hasText: 'Section AGeneral' }).getByPlaceholder('Enter Postal Code').fill('12560');
   await page.locator('section-card').filter({ hasText: 'Section AGeneral' }).getByRole('combobox').selectOption('Sri Lanka');
   await page.locator('section-card').filter({ hasText: 'Section AGeneral' }).getByPlaceholder('Enter Name').click();
-  await page.locator('section-card').filter({ hasText: 'Section AGeneral' }).getByPlaceholder('Enter Name').fill('Tester');
+  await page.locator('section-card').filter({ hasText: 'Section AGeneral' }).getByPlaceholder('Enter Name').fill('Brandon Stark');
   await page.getByRole('textbox', { name: 'Enter Designation' }).click();
   await page.getByRole('textbox', { name: 'Enter Designation' }).fill('CEO');
   await page.locator('subsection').filter({ hasText: 'Contact Person Information' }).getByPlaceholder('Enter Email Address').click();
-  await page.locator('subsection').filter({ hasText: 'Contact Person Information' }).getByPlaceholder('Enter Email Address').fill('test@mail.com');
+  await page.locator('subsection').filter({ hasText: 'Contact Person Information' }).getByPlaceholder('Enter Email Address').fill('stark@mail.com');
   await page.getByRole('textbox', { name: 'Enter Phone Number' }).click();
-  await page.getByRole('textbox', { name: 'Enter Phone Number' }).fill('71564789');
-  await page.getByRole('textbox', { name: 'Write..' }).click();
-  await page.getByRole('textbox', { name: 'Write..' }).fill('Test test test test test test test');
+  await page.getByRole('textbox', { name: 'Enter Phone Number' }).fill('77564789');
+  await page.getByRole('textbox', { name: 'Enter Organization Name' }).click();
+  await page.getByRole('textbox', { name: 'Enter Organization Name' }).fill('New Sakura Pvt Ltd');
+  await page.locator('section-card').filter({ hasText: 'Section BCertificate' }).getByPlaceholder('Enter Address Line 1').click();
+  await page.locator('section-card').filter({ hasText: 'Section BCertificate' }).getByPlaceholder('Enter Address Line 1').fill('Battaramulla');
+  await page.locator('section-card').filter({ hasText: 'Section BCertificate' }).getByPlaceholder('Enter City').click();
+  await page.locator('section-card').filter({ hasText: 'Section BCertificate' }).getByPlaceholder('Enter City').click();
+  await page.locator('section-card').filter({ hasText: 'Section BCertificate' }).getByPlaceholder('Enter City').fill('Colombo');
+  await page.locator('section-card').filter({ hasText: 'Section BCertificate' }).getByPlaceholder('Enter State/Province/Region').click();
+  await page.locator('section-card').filter({ hasText: 'Section BCertificate' }).getByPlaceholder('Enter State/Province/Region').fill('Western');
+  await page.locator('section-card').filter({ hasText: 'Section BCertificate' }).getByPlaceholder('Enter Postal Code').click();
+  await page.locator('section-card').filter({ hasText: 'Section BCertificate' }).getByPlaceholder('Enter Postal Code').fill('12500');
+  await page.locator('section-card').filter({ hasText: 'Section BCertificate' }).getByRole('combobox').selectOption('Sri Lanka');
+  await page.getByRole('textbox', { name: 'Write..', exact: true }).click();
+  await page.getByRole('textbox', { name: 'Write..', exact: true }).fill('Test test test test');
   await page.locator('form-field-wrapper').filter({ hasText: 'Stated Capital' }).getByPlaceholder('0').click();
   await page.locator('form-field-wrapper').filter({ hasText: 'Stated Capital' }).getByPlaceholder('0').fill('100000000');
   await page.locator('form-field-wrapper').filter({ hasText: 'Shareholders Funds' }).getByPlaceholder('0').click();
@@ -67,6 +79,27 @@ test('Apply Credit Rating Agency license with valid data', async ({ page }) => {
   await page.getByRole('textbox', { name: 'Name of The Company' }).fill('XYZ');
   await page.getByRole('combobox').selectOption('Partnership');
   await page.getByRole('button', { name: 'Save' }).click();
+
+  await page.locator('subsection').filter({ hasText: 'Ultimate Holding CompanyIf' }).getByPlaceholder('Enter Name').click();
+  await page.locator('subsection').filter({ hasText: 'Ultimate Holding CompanyIf' }).getByPlaceholder('Enter Name').fill('N/A');
+  await page.locator('form-field-wrapper').filter({ hasText: 'Country of Incorporation' }).getByRole('combobox').selectOption('N/A');
+  await page.getByRole('textbox', { name: 'Write...' }).click();
+  await page.getByRole('textbox', { name: 'Write...' }).fill('N/A');
+  await page.locator('field-group').filter({ hasText: 'Registered Office Address' }).getByPlaceholder('Enter Address Line 1').click();
+  await page.locator('field-group').filter({ hasText: 'Registered Office Address' }).getByPlaceholder('Enter Address Line 1').fill('N/A');
+  await page.locator('field-group').filter({ hasText: 'Registered Office Address' }).getByPlaceholder('Enter City').click();
+  await page.locator('field-group').filter({ hasText: 'Registered Office Address' }).getByPlaceholder('Enter City').fill('N/A');
+  await page.locator('field-group').filter({ hasText: 'Registered Office Address' }).getByPlaceholder('Enter State/Province/Region').click();
+  await page.locator('field-group').filter({ hasText: 'Registered Office Address' }).getByPlaceholder('Enter State/Province/Region').fill('N/A');
+  await page.locator('field-group').filter({ hasText: 'Registered Office Address' }).getByPlaceholder('Enter Postal Code').click();
+  await page.locator('field-group').filter({ hasText: 'Registered Office Address' }).getByPlaceholder('Enter Postal Code').fill('N/A');
+  await page.locator('field-group').filter({ hasText: 'Registered Office Address' }).getByRole('combobox').selectOption('Sri Lanka');
+  await page.getByRole('textbox', { name: 'Name', exact: true }).click();
+  await page.getByRole('textbox', { name: 'Name', exact: true }).fill('N/A');
+  await page.getByRole('textbox', { name: 'Name of Shareholder' }).click();
+  await page.getByRole('textbox', { name: 'Name of Shareholder' }).fill('Kusal Silva');
+  await page.getByRole('spinbutton').click();
+  await page.getByRole('spinbutton').fill('10');
 
   const filePath = 'C:/Users/MSI/Downloads/postman.jpg';
 
@@ -95,22 +128,26 @@ test('Apply Credit Rating Agency license with valid data', async ({ page }) => {
 
   // Page 2
 
-  await page.getByRole('radio', { name: 'No' }).check();
-  await page.locator('date-picker i').click();
-  await page.getByLabel('Saturday, February 28,').getByText('28').click();
-  await page.locator('subsection').filter({ hasText: 'Odinary Shareholding Type' }).getByRole('combobox').selectOption('Individual');
-  await page.getByRole('row', { name: 'Individual ' }).getByPlaceholder('Shareholder Name').click();
-  await page.getByRole('row', { name: 'Individual ' }).getByPlaceholder('Shareholder Name').fill('Thisara Perera');
-  await page.getByRole('row', { name: 'Individual Thisara Perera ' }).getByPlaceholder('No. of Shares').click();
-  await page.getByRole('row', { name: 'Individual Thisara Perera ' }).getByPlaceholder('No. of Shares').fill('80');
-  await page.getByRole('row', { name: 'Individual Thisara Perera ' }).getByPlaceholder('Percentage of Shares Held').click();
-  await page.getByRole('row', { name: 'Individual Thisara Perera ' }).getByPlaceholder('Percentage of Shares Held').fill('100');
-  await page.getByRole('row', { name: '1 ' }).getByPlaceholder('Shareholder Name').click();
-  await page.getByRole('row', { name: '1 ' }).getByPlaceholder('Shareholder Name').fill('Thusith');
-  await page.getByRole('row', { name: 'Thusith ' }).getByPlaceholder('No. of Shares').click();
-  await page.getByRole('row', { name: 'Thusith ' }).getByPlaceholder('No. of Shares').fill('80');
-  await page.getByPlaceholder('0').click();
-  await page.getByPlaceholder('0').fill('100');
+  const page2 = page
+  .locator('form-field-wrapper')
+  .filter({ hasText: 'Group Structure' });
+
+  await expect(page2).toBeVisible();
+  const fileInput = page.locator('input[type="file"]').nth(0);
+  await fileInput.setInputFiles(filePath);
+  await page.locator('input[type="file"]').nth(1)
+    .setInputFiles(filePath);
+  
+  // await page.getByRole('radio', { name: 'No' }).check();
+  // await page.locator('subsection').filter({ hasText: 'Odinary Shareholding Type' }).getByRole('combobox').selectOption('Individual');
+  // await page.getByRole('row', { name: 'Individual ' }).getByPlaceholder('Shareholder Name').click();
+  // await page.getByRole('row', { name: 'Individual ' }).getByPlaceholder('Shareholder Name').fill('Thisara Perera');
+  // await page.getByRole('row', { name: 'Individual Thisara Perera ' }).getByPlaceholder('No. of Shares').click();
+  // await page.getByRole('row', { name: 'Individual Thisara Perera ' }).getByPlaceholder('No. of Shares').fill('80');
+  // await page.getByRole('row', { name: 'Individual Thisara Perera ' }).getByPlaceholder('Percentage of Shares Held').click();
+  // await page.getByRole('row', { name: 'Individual Thisara Perera ' }).getByPlaceholder('Percentage of Shares Held').fill('100');
+  // await page.locator('div:nth-child(4) > subsection > .pt-4 > table-entry-section-content > div > .table-wrapper > .fields-entry-table > tbody > tr > .text-end > .bi').click();
+  await page.locator('.pt-4.p-4 > table-entry-section-content > div > .table-wrapper > .fields-entry-table > tbody > tr > .text-end > .bi').click();
   await page.waitForTimeout(2000);
   await page.getByRole('button', { name: 'Next' }).click();
 
@@ -156,49 +193,46 @@ test('Apply Credit Rating Agency license with valid data', async ({ page }) => {
     await question.locator('input[type="radio"]').nth(optionIndex).check();
   }
 
+  // No
+  await selectRadio(page, 'rejected by the SEC', 0);
+
+  // Yes
+  await selectRadio(page, 'Rules of the SEC', 1);
+
+  await page.getByRole('textbox', { name: 'Name of the Director' }).click();
+  await page.getByRole('textbox', { name: 'Name of the Director' }).fill('Person A');
+  await page.getByRole('textbox', { name: 'Company' }).click();
+  await page.getByRole('textbox', { name: 'Company' }).fill('ABC');
+  await page.getByRole('combobox').selectOption('Investment Manager');
+
   await selectRadio(page, 'fraudulently or dishonestly', 0);
   await selectRadio(page, 'sanction under the SEC Act', 0);
   await selectRadio(page, 'securities outside Sri Lanka', 0);
   await selectRadio(page, 'discharged or undischarged bankrupts', 0);
-  await selectRadio(page, 'Is an undischarged bankrupt whether within or outside Sri Lanka', 0);
+  await selectRadio(page, ' Is an undischarged bankrupt, either within or outside Sri Lanka', 0);
 
   await page.waitForTimeout(2000);
   await page.getByRole('button', { name: 'Next' }).click();
 
   // Page 4
-  await selectRadio(page, 'in any criminal/civil proceedings, or are there any proceedings now pending in a court of law?', 0);
+  await selectRadio(page, 'Are there any such proceedings now pending in a court of law', 0);
   await page.waitForTimeout(1000);
-  await selectRadio(page, 'an equivalent person appointed in respect of any of the assets of the Applicant?', 1);
+  await selectRadio(page, 'in respect of any of the assets of the Applicant', 1);
   await page.waitForTimeout(1000);
   await page.getByRole('textbox', { name: 'Details' }).click();
-  await page.getByRole('textbox', { name: 'Details' }).fill('Test test test test');
+  await page.getByRole('textbox', { name: 'Details' }).fill('Test');
 
-  await selectRadio(page, 'Is the Applicant in the course of being wound up or otherwise dissolved or an undischarged bankrupt?', 0);
-  await selectRadio(page, 'a judgment debt against the Applicant which has not been satisfied in whole or in part?', 0);
-  await selectRadio(page, 'the Applicant entered into a compromise or a scheme of arrangement with its creditors?', 0);
-  await selectRadio(page, 'the Applicant been subjected to any form of disciplinary proceedings/ actions by the SEC or any other professional or regulatory body?', 0);
+  await selectRadio(page, 'process of being wound up, dissolved, or an undischarged bankrupt', 0);
+  await selectRadio(page, 'satisfied in whole or in part', 0);
+  await selectRadio(page, 'Compromise or Scheme of Arrangement with its Creditors', 0);
+  await selectRadio(page, 'actions by the SEC or any other professional or regulatory body', 0);
+  await selectRadio(page, 'as a Market Intermediary and been rejected by the SEC', 0);
+  await selectRadio(page, 'renew its licence in terms of the applicable Rules of the SEC', 0);
 
   await page.waitForTimeout(2000);
   await page.getByRole('button', { name: 'Next' }).click();
 
   // Page 5
-    // Memorandum / Articles of
-    // await page.locator('subsection')
-    // .filter({ hasText: 'Memorandum/ Articles of' })
-    // .locator('img')
-    // .click();
-
-    // const fileInput2 = page.locator('input[type="file"]');
-    // await fileInput2.first().setInputFiles(filePath);
-
-
-    // // Certificate of Incorporation
-    // await page.locator('subsection')
-    // .filter({ hasText: 'Certificate of Incorporation' })
-    // .locator('img')
-    // .click();
-
-    // await fileInput2.nth(1).setInputFiles(filePath);
 
   await page.getByRole('textbox', { name: 'Regulator' }).click();
   await page.getByRole('textbox', { name: 'Regulator' }).fill('ABC');
@@ -210,14 +244,6 @@ test('Apply Credit Rating Agency license with valid data', async ({ page }) => {
   for (let i = 0; i < await uploads.count(); i++) {
     await uploads.nth(i).setInputFiles(filePath);
   }
-
-  await page.getByRole('textbox', { name: 'Full Name' }).fill('Sasindu Rashmika');
-  await page.getByRole('textbox', { name: 'NIC/Passport No.' }).fill('200130002123');
-  await page.getByRole('cell', { name: 'Select Designation' }).getByRole('combobox').selectOption('CEO');
-  await page.locator('.bi.bi-calendar-event').click();
-  await page.getByLabel('Saturday, February 28,').getByText('28').click();
-  await page.getByText('Attach File').click();
-  await page.getByLabel('Attach File').setInputFiles(filePath);
 
   await page.waitForTimeout(2000); 
   await page.getByRole('button', { name: 'Submit' }).click();
